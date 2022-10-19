c810023330330a2eaeb83ed106c8c74f501b8947
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2022-10-18 21:37:57 -0300

    Desafio finalizado.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -15,8 +15,9 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o nome do Cliente !");
         String nomeCliente = scanner.next();
 
+        System.out.println("Por favor, digite o valor a sacar da sua conta!");
         double saldo = scanner.nextDouble();
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + "seu saldo " + saldo + " já está disponível para saque");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " seu saldo " + saldo + " já está disponível para saque");
 
 
 
